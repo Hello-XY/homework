0e602cc2e50337394bcbb08d25305a8f5b67d8db
Hello-XY <1661230401@qq.com>
2022-06-27 21:11:03 +0800

    处理less文件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import './index'
 import './style/index.css'
+import './style/index.less';
 
 // jq的使用
 // src/mains.js
 // 先引入jquery
 import $ from 'jquery';
-$('#swiper').css('background-color', 'red')
+// $('#swiper').css('background-color', 'red')
